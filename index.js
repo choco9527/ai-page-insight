40e27d5079dd9e1ede9da05288b244d6ca106b01
wuzk <wuzk@flatincbr.com>
2023-09-19 17:36:05 +0800

    feat: 初始化

--- index.js
+++ index.js
@@ -24,16 +24,7 @@ const main = async function () {
   }
   try {
     const page = await openBrowser() // 打开浏览器
-    await login(page) // 登录
-    await sleep(3000); // 等待3s进入首页 仅等待是不好确保的，需要做更细致的区分，此处简化
-
-    console.log('监听需要的响应')
-    await listen(page)
-    await sleep(1000)
-
-    console.log('触发事件如点击tab')
-    await emit(page)
-    await sleep(3000)
+    await openPage(page) // 打开页面
 
 
     async function openBrowser() {
@@ -51,65 +42,39 @@ const main = async function () {
       }
       const browser = await puppeteer.launch(options);
       const [p] = await browser.pages()
-      return p // 递交page
+      return p
     }
 
-    async function login(p) {
-      if (process.env.EMAIL && process.env.PASSWORD) { // 输入账号密码
-        console.log('正在登录巨量')
-        console.log(process.env.EMAIL);
-
-        const urls = [
-          'https://business.oceanengine.com/login?appKey=51' // 巨量登录页
-        ]
-        const url = urls[0]
-        await p.setBypassCSP(true)
-        await p.goto(url);
-
-        await p.waitForSelector('.login-bg')
-
-        await sleep(1000);
-
-        // 使用坐标点击最稳妥
-        const x = 950 // 纵坐标相同
-        await p.mouse.click(x, 345) // 账号
-        await p.keyboard.type(process.env.EMAIL, {delay: 80})
-        await p.mouse.click(x, 393) // 密码
-        await p.keyboard.type(process.env.PASSWORD, {delay: 80})
-
-        await p.click('.check-box-container', {delay: 200}) // 协议
-
-        const submit = async () => { // 登录
-          await p.click('.account-center-submit', {delay: 200})
-        }
-
-        // 处理验证码
-        await calculateDistance(p, submit)
-
-      } else {
-        throw new Error('没有账号密码')
-      }
+    /**
+     * 打开页面
+     * @param p
+     * @returns {Promise<void>}
+     */
+    async function openPage(p) {
+      const urls = [
+        'https://www.bilibili.com/' //
+      ]
+      const url = urls[0]
+      await p.setBypassCSP(true)
+      await p.goto(url);
     }
 
-    async function listen(p) {
-      // 开启响应监听
-      await p.on('response', async res => {
-        const fnMap = {
-          getLiveRoomData, // 直播 -> 直播数据汇总
-          getLivingData, // 直播 -> 正在直播
-          getAnalysisLivingData // 分析 -> 直播分析 -> 直播间
-        };
-        Object.keys(dataMap).forEach(name => {
-          fnMap[name](res).then(data => {
-            if (data) dataMap[name] = data
-          })
-        })
-      })
+    /**
+     * 通过opencv获取视频内容
+     * @param p
+     * @returns {Promise<void>}
+     */
+    async function getVideoInfo(p) {
+
     }
 
-    async function emit(p) {
-      await liveClick(p)
-      await analysisClick(p)
+    /**
+     * 上传openai获取内容
+     * @param p
+     * @returns {Promise<void>}
+     */
+    async function emitOpenAi(p) {
+
     }
 
     return dataMap
